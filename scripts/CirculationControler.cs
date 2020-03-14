eb6d1e16e531c5ea37dcc719080a7a7dae29cc8f
Pawlost <mr.p.balusek@gmail.com>
2020-03-14 15:52:14 +0100

    Switched to Godot version 3.2.1

--- scripts/CirculationControler.cs
+++ scripts/CirculationControler.cs
@@ -1,64 +1,64 @@
 using Godot;
 public class CirculationControler : Controler {
-    private Slider windIntesitySlider;
-    private SpinBox windIntesityBox;
+	private Slider windIntesitySlider;
+	private SpinBox windIntesityBox;
 
-    private Slider windRangeSlider;
-    private SpinBox windRangeBox;
+	private Slider windRangeSlider;
+	private SpinBox windRangeBox;
 
-    private Slider pressureAtSeaLevelSlider;
-    private SpinBox pressureAtSeaLevelBox;
-    public override void _Ready () {
-        FindRoot ();
+	private Slider pressureAtSeaLevelSlider;
+	private SpinBox pressureAtSeaLevelBox;
+	public override void _Ready () {
+		FindRoot ();
 
-        windIntesitySlider = (Slider) FindNode ("Wind Intesity").GetChild (1);
-        windIntesityBox = (SpinBox) FindNode ("Wind Intesity").GetChild (2);
+		windIntesitySlider = (Slider) FindNode ("Wind Intesity").GetChild (1);
+		windIntesityBox = (SpinBox) FindNode ("Wind Intesity").GetChild (2);
 
-        windRangeSlider = (Slider) FindNode ("Wind Range").GetChild (1);
-        windRangeBox = (SpinBox) FindNode ("Wind Range").GetChild (2);
+		windRangeSlider = (Slider) FindNode ("Wind Range").GetChild (1);
+		windRangeBox = (SpinBox) FindNode ("Wind Range").GetChild (2);
 
-        pressureAtSeaLevelSlider = (Slider) FindNode ("Pressure at sea level").GetChild (1);
-        pressureAtSeaLevelBox = (SpinBox) FindNode ("Pressure at sea level").GetChild (2);
+		pressureAtSeaLevelSlider = (Slider) FindNode ("Pressure at sea level").GetChild (1);
+		pressureAtSeaLevelBox = (SpinBox) FindNode ("Pressure at sea level").GetChild (2);
 
-        windIntesitySlider.SetValue (config.circulation.wind_intensity);
-        windIntesityBox.SetValue (config.circulation.wind_intensity);
+		windIntesitySlider.SetValue (config.circulation.wind_intensity);
+		windIntesityBox.SetValue (config.circulation.wind_intensity);
 
-        windRangeSlider.SetValue (config.circulation.wind_range);
-        windRangeBox.SetValue (config.circulation.wind_range);
+		windRangeSlider.SetValue (config.circulation.wind_range);
+		windRangeBox.SetValue (config.circulation.wind_range);
 
-        pressureAtSeaLevelSlider.SetValue (config.circulation.pressure_at_sea_level);
-        pressureAtSeaLevelBox.SetValue (config.circulation.pressure_at_sea_level);
+		pressureAtSeaLevelSlider.SetValue (config.circulation.pressure_at_sea_level);
+		pressureAtSeaLevelBox.SetValue (config.circulation.pressure_at_sea_level);
 
-        windIntesityBox.Connect ("value_changed", this, "BoxTriggered");
-        windRangeBox.Connect ("value_changed", this, "BoxTriggered");
-        pressureAtSeaLevelBox.Connect ("value_changed", this, "BoxTriggered");
+		windIntesityBox.Connect ("value_changed", this, "BoxTriggered");
+		windRangeBox.Connect ("value_changed", this, "BoxTriggered");
+		pressureAtSeaLevelBox.Connect ("value_changed", this, "BoxTriggered");
 
-        windIntesitySlider.Connect ("value_changed", this, "SliderTriggered");
-        windRangeSlider.Connect ("value_changed", this, "SliderTriggered");
-        pressureAtSeaLevelSlider.Connect ("value_changed", this, "SliderTriggered");
-    }
+		windIntesitySlider.Connect ("value_changed", this, "SliderTriggered");
+		windRangeSlider.Connect ("value_changed", this, "SliderTriggered");
+		pressureAtSeaLevelSlider.Connect ("value_changed", this, "SliderTriggered");
+	}
 
-    public override void BoxTriggered (int value) {
-        config.circulation.wind_intensity = windIntesityBox.GetValue ();
-        config.circulation.wind_range = (int) windRangeBox.GetValue ();
-        config.circulation.pressure_at_sea_level = (int) pressureAtSeaLevelBox.GetValue ();
+	public override void BoxTriggered (int value) {
+		config.circulation.wind_intensity = (double)windIntesityBox.GetValue ();
+		config.circulation.wind_range = (int) windRangeBox.GetValue ();
+		config.circulation.pressure_at_sea_level = (int) pressureAtSeaLevelBox.GetValue ();
 
-        windIntesitySlider.SetValue (windIntesityBox.GetValue ());
-        windRangeSlider.SetValue (windRangeBox.GetValue ());
-        pressureAtSeaLevelSlider.SetValue (pressureAtSeaLevelBox.GetValue ());
+		windIntesitySlider.SetValue (windIntesityBox.GetValue ());
+		windRangeSlider.SetValue (windRangeBox.GetValue ());
+		pressureAtSeaLevelSlider.SetValue (pressureAtSeaLevelBox.GetValue ());
 
-        root.Generate ();
-    }
+		root.Generate ();
+	}
 
-    public override void SliderTriggered (int value) {
-        config.circulation.wind_intensity = windIntesitySlider.GetValue ();
-        config.circulation.wind_range = (int) windRangeSlider.GetValue ();
-        config.circulation.pressure_at_sea_level = (int) pressureAtSeaLevelSlider.GetValue ();
+	public override void SliderTriggered (int value) {
+		config.circulation.wind_intensity = windIntesitySlider.GetValue ();
+		config.circulation.wind_range = (int) windRangeSlider.GetValue ();
+		config.circulation.pressure_at_sea_level = (int) pressureAtSeaLevelSlider.GetValue ();
 
-        windIntesityBox.SetValue (windIntesitySlider.GetValue ());
-        windRangeBox.SetValue (windRangeSlider.GetValue ());
-        pressureAtSeaLevelBox.SetValue (pressureAtSeaLevelSlider.GetValue ());
+		windIntesityBox.SetValue (windIntesitySlider.GetValue ());
+		windRangeBox.SetValue (windRangeSlider.GetValue ());
+		pressureAtSeaLevelBox.SetValue (pressureAtSeaLevelSlider.GetValue ());
 
-        root.Generate ();
-    }
+		root.Generate ();
+	}
 }
--- scripts/HumidityControler.cs
+++ scripts/HumidityControler.cs
@@ -1,50 +1,50 @@
 using Godot;
 public class HumidityControler : Controler {
-    private Slider transpirationSlider;
-    private SpinBox transpirationBox;
+	private Slider transpirationSlider;
+	private SpinBox transpirationBox;
 
-    private Slider evaporationSlider;
-    private SpinBox evaporationBox;
+	private Slider evaporationSlider;
+	private SpinBox evaporationBox;
 
-    public override void _Ready () {
-        FindRoot ();
+	public override void _Ready () {
+		FindRoot ();
 
-        transpirationSlider = (Slider) FindNode ("Transpiration").GetChild (1);
-        transpirationBox = (SpinBox) FindNode ("Transpiration").GetChild (2);
+		transpirationSlider = (Slider) FindNode ("Transpiration").GetChild (1);
+		transpirationBox = (SpinBox) FindNode ("Transpiration").GetChild (2);
 
-        evaporationSlider = (Slider) FindNode ("Evaporation").GetChild (1);
-        evaporationBox = (SpinBox) FindNode ("Evaporation").GetChild (2);
+		evaporationSlider = (Slider) FindNode ("Evaporation").GetChild (1);
+		evaporationBox = (SpinBox) FindNode ("Evaporation").GetChild (2);
 
-        transpirationSlider.SetValue (config.humidity.transpiration);
-        transpirationBox.SetValue (config.humidity.transpiration);
+		transpirationSlider.SetValue (config.humidity.transpiration);
+		transpirationBox.SetValue (config.humidity.transpiration);
 
-        evaporationSlider.SetValue (config.humidity.evaporation);
-        evaporationBox.SetValue (config.humidity.evaporation);
+		evaporationSlider.SetValue (config.humidity.evaporation);
+		evaporationBox.SetValue (config.humidity.evaporation);
 
-        transpirationBox.Connect ("value_changed", this, "BoxTriggered");
-        evaporationBox.Connect ("value_changed", this, "BoxTriggered");
+		transpirationBox.Connect ("value_changed", this, "BoxTriggered");
+		evaporationBox.Connect ("value_changed", this, "BoxTriggered");
 
-        transpirationSlider.Connect ("value_changed", this, "SliderTriggered");
-        evaporationSlider.Connect ("value_changed", this, "SliderTriggered");
-    }
+		transpirationSlider.Connect ("value_changed", this, "SliderTriggered");
+		evaporationSlider.Connect ("value_changed", this, "SliderTriggered");
+	}
 
-    public override void BoxTriggered (int value) {
-        config.humidity.transpiration = transpirationBox.GetValue ();
-        config.humidity.evaporation = evaporationBox.GetValue ();
+	public override void BoxTriggered (int value) {
+		config.humidity.transpiration = transpirationBox.GetValue ();
+		config.humidity.evaporation = evaporationBox.GetValue ();
 
-        transpirationSlider.SetValue (transpirationBox.GetValue ());
-        evaporationSlider.SetValue (evaporationBox.GetValue ());
+		transpirationSlider.SetValue (transpirationBox.GetValue ());
+		evaporationSlider.SetValue (evaporationBox.GetValue ());
 
-        root.Generate ();
-    }
+		root.Generate ();
+	}
 
-    public override void SliderTriggered (int value) {
-        config.humidity.transpiration = transpirationSlider.GetValue ();
-        config.humidity.evaporation = evaporationSlider.GetValue ();
+	public override void SliderTriggered (int value) {
+		config.humidity.transpiration = transpirationSlider.GetValue ();
+		config.humidity.evaporation = evaporationSlider.GetValue ();
 
-        transpirationSlider.SetValue (transpirationSlider.GetValue ());
-        evaporationSlider.SetValue (evaporationSlider.GetValue ());
+		transpirationSlider.SetValue (transpirationSlider.GetValue ());
+		evaporationSlider.SetValue (evaporationSlider.GetValue ());
 
-        root.Generate ();
-    }
+		root.Generate ();
+	}
 }
